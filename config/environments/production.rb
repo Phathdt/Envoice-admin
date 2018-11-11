109c8f99e137f9e3db6779ad89e436acec341b06
Phathdt <phathdt.leviateam@gmail.com>
2018-11-11 13:21:18 +0700

    use s3 amz

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.active_job.queue_adapter = :sidekiq
+  config.active_storage.service = :amazon
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
