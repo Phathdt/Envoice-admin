1f844863da04336cd90d47e311d26225c19613c6
Phathdt <phathdt.leviateam@gmail.com>
2018-12-02 10:26:02 +0700

    remove image pdf

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,7 +4,6 @@ class Invoice < ApplicationRecord
   enum state: { processing: 0, approved: 1 }
   enum nem_validate: { pending: 0, validated: 1, not_validated: 2 }
 
-  has_one_attached :image
   has_one_attached :pdf
 
   after_create :check_nem_validate
